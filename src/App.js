5154b31bdf8b487403fb33f1e62f187e43058022
ayonshafiul <ayonshafiul@gmail.com>
2022-09-29 22:28:59 +0600

    Create simple state management using redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/actions";
 function App() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const actions = bindActionCreators(actionCreators, dispatch);
+  console.log(state);
+  console.log(actions);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => actions.depositMoney(100)}>Deposit 100</button>
+      <button onClick={() => actions.withdrawMoney(100)}>withdraw 100</button>
+      <div>{state.account}</div>
     </div>
   );
 }
---
+++ src/state/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+import accountReducer from "./accountReducer";
+
+const reducers = combineReducers({
+  account: accountReducer
+})
+
+
+export default reducers;
